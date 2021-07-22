4a4f8421c2267e59f46fde2a8a38aaed2926f16c
NUR UZ ZAMAN <48312903+nur-zaman@users.noreply.github.com>
2021-07-23 03:22:31 +0600

    new features
    added styles, visualizations , fixed midpoint circle algorithm

--- algorithms.js
+++ algorithms.js
@@ -1,81 +1,81 @@
-const ddaRUN = document.getElementById("dda-run") 
-const midpointRUN = document.getElementById("midpoint-run") 
-const circleRUN = document.getElementById("circle-run") 
+const ddaRUN = document.getElementById("dda-run");
+const midpointRUN = document.getElementById("midpoint-run");
+const circleRUN = document.getElementById("circle-run");
 
-const results = document.getElementById("results") 
+const results = document.getElementById("results");
 
-let DDAX = [] 
-let DDAY = [] 
-let MIDX = [] 
-let MIDY = [] 
-// let cirX=[]
-// let cirY=[]
-let dir = [] 
-let d = [] 
-let m 
+let DDAX = [];
+let DDAY = [];
+let MIDX = [];
+let MIDY = [];
+let circX = [];
+let circY = [];
+let dir = [];
+let d = [];
+let m;
+
 ddaRUN.onclick = () => {
-  DDAX = [] 
-  DDAY = [] 
-  m = 0
-  DDA() 
-  showResultsDDA() 
-} 
+  DDAX = [];
+  DDAY = [];
+  m = 0;
+  DDA();
+  showResultsDDA();
+  plotValues(DDAX, DDAY);
+};
 
 midpointRUN.onclick = () => {
-  MIDX = [] 
-  MIDY = [] 
-  dir = [] 
-  d = [] 
-  midpointLine() 
-  showResultsMidpoint() 
-} 
-
+  MIDX = [];
+  MIDY = [];
+  dir = [];
+  d = [];
+  midpointLine();
+  showResultsMidpoint();
+  plotValues(MIDX, MIDY);
+};
 
 circleRUN.onclick = () => {
-  MIDX = [] 
-  MIDY = [] 
-  dir = [] 
-  d = [] 
-  midPointCircle() 
-  showResultsCircle() 
-} 
+  circX = [];
+  circY = [];
+  dir = [];
+  d = [];
+ let center = midPointCircle();
+
+  showResultsCircle(center);
+  plotCircle(circX, circY,center);
+};
 
 function drawPoints(x, y, pointX, pointY) {
-  pointX.push(x) 
-  pointY.push(y) 
+  pointX.push(x);
+  pointY.push(y);
 }
 
 function pushDirection(direction, dValue) {
-  dir.push(direction) 
-  d.push(dValue) 
+  dir.push(direction);
+  d.push(dValue);
 }
 
 function DDA() {
-  let x1 = Number(document.getElementById("dda-x1").value) 
-  let x2 = Number(document.getElementById("dda-x2").value) 
-  let y1 = Number(document.getElementById("dda-y1").value) 
-  let y2 = Number(document.getElementById("dda-y2").value) 
-  //   console.log(''+x1+' '+x2+' '+y1+' '+y2)
+  let x1 = Number(document.getElementById("dda-x1").value);
+  let x2 = Number(document.getElementById("dda-x2").value);
+  let y1 = Number(document.getElementById("dda-y1").value);
+  let y2 = Number(document.getElementById("dda-y2").value);
 
-  
-  m = (y2 - y1) / (x2 - x1) 
-  console.log("m : " + m) 
-  // console.log(DDAX)
-  drawPoints(x1, y1, DDAX, DDAY) 
+  m = (y2 - y1) / (x2 - x1);
+
+  drawPoints(x1, y1, DDAX, DDAY);
   if (m > -1 && m < 1) {
     while (x1 < x2) {
-      x1 += 1 
-      y1 += m 
-      // console.log(this.DDAX)
-      drawPoints(x1, Math.round(y1), DDAX, DDAY) 
+      x1 += 1;
+      y1 += m;
+      drawPoints(x1, Math.round(y1), DDAX, DDAY);
     }
   } else {
-    if (m != Infinity) mInv = 1 / m 
-    else mInv = 0 
+    if (m != Infinity) mInv = 1 / m;
+    else mInv = 0;
     while (y1 < y2) {
-      x1 += mInv 
-      y1 += 1 
-      drawPoints(Math.round(x1), y1, DDAX, DDAY) 
+      x1 += mInv;
+      y1 += 1;
+      drawPoints(Math.round(x1), y1, DDAX, DDAY);
     }
   }
 }
@@ -84,47 +84,47 @@ function showResultsDDA() {
   results.innerHTML = `<tr>
   <th>Points</th>
   <th>m</th>
-</tr>` 
-  for (let i = 0;  i < DDAX.length;  i++) {
-    results.innerHTML += `<tr> <td>(${DDAX[i]},${DDAY[i]})</td><td>${m}</td> </tr>` 
+</tr>`;
+  for (let i = 0; i < DDAX.length; i++) {
+    results.innerHTML += `<tr> <td>(${DDAX[i]},${DDAY[i]})</td><td>${m}</td> </tr>`;
   }
 }
 
 function midpointLine() {
-  let dx, dy, incrE, incrNE, d, x, y 
-  let x1 = Number(document.getElementById("midp-x1").value) 
-  let x2 = Number(document.getElementById("midp-x2").value) 
-  let y1 = Number(document.getElementById("midp-y1").value) 
-  let y2 = Number(document.getElementById("midp-y2").value) 
+  let dx, dy, incrE, incrNE, d, x, y;
+  let x1 = Number(document.getElementById("midp-x1").value);
+  let x2 = Number(document.getElementById("midp-x2").value);
+  let y1 = Number(document.getElementById("midp-y1").value);
+  let y2 = Number(document.getElementById("midp-y2").value);
 
-  dx = x2 - x1 
-  dy = y2 - y1 
-  m=dy/dx
-  d = 2 * dy - dx 
-  
-  pushDirection("init", d) 
-  incrE = 2 * dy 
-  incrNE = 2 * (dy - dx) 
-  x = x1 
-  y = y1 
+  dx = x2 - x1;
+  dy = y2 - y1;
+  m = dy / dx;
+  d = 2 * dy - dx;
 
-  drawPoints(x, y, MIDX, MIDY) 
+  pushDirection("init", d);
+  incrE = 2 * dy;
+  incrNE = 2 * (dy - dx);
+  x = x1;
+  y = y1;
+
+  drawPoints(x, y, MIDX, MIDY);
 
   while (x < x2) {
     if (d <= 0) {
       //choose E
-      d = d + incrE 
-      x = x + 1 
-      pushDirection("E", d) 
+      d = d + incrE;
+      x = x + 1;
+      pushDirection("E", d);
     } else {
       //choose NE
-      d = d + incrNE 
-      x = x + 1 
-      y = y + 1 
-      pushDirection("NE", d) 
+      d = d + incrNE;
+      x = x + 1;
+      y = y + 1;
+      pushDirection("NE", d);
     }
 
-    drawPoints(x, y, MIDX, MIDY)  //The selected pixel closest to the line
+    drawPoints(x, y, MIDX, MIDY); //The selected pixel closest to the line
   }
 }
 
@@ -133,61 +133,54 @@ function showResultsMidpoint() {
   <th>Points</th>
   <th>Direction</th>
   <th>d</th>
-</tr>` 
-  for (let i = 0 ; i < MIDX.length ; i++) {
+</tr>`;
+  for (let i = 0; i < MIDX.length; i++) {
     results.innerHTML += `<tr> <td>(${MIDX[i]},${MIDY[i]})</td> 
     <td>${dir[i]}</td>
     <td>${d[i]}</td>
-    </tr>`
+    </tr>`;
   }
 }
 
-
 function midPointCircle() {
+  let x1 = Number(document.getElementById("circle-x1").value);
+  let y1 = Number(document.getElementById("circle-y1").value);
+  let radius = Number(document.getElementById("circle-r").value);
+  let d = 1 - radius;
 
-  let x1 = Number(document.getElementById("circle-x1").value) 
-  let y1 = Number(document.getElementById("circle-y1").value) 
-  let radius = Number(document.getElementById("circle-r").value) 
-    let d = 1 - radius
-    
-    let x = x1
-    let y =y1
-    x = 0;
-    
-    y = radius;
-    
-    drawPoints(x+x1, y+y1, MIDX, MIDY)
-    pushDirection("init", d) 
-    while (x < y) {
-    
+  let x 
+  let y
+  x = 0;
+
+  y = radius;
+
+  drawPoints(x , y , circX, circY);
+  pushDirection("init", d);
+  while (x < y) {
     if (d < 0) {
-    
-    //choose E
-    
-    d = d + 2*x + 3;
-    
-    x = x + 1;
-    pushDirection("E", d) 
+      //choose E
+
+      d = d + 2 * x + 3;
+
+      x = x + 1;
+      pushDirection("E", d);
+    } else {
+      //choose SE
+
+      d = d + 2 * x - 2 * y + 5;
+
+      x = x + 1;
+
+      y = y - 1;
+      pushDirection("SE", d);
     }
-    
-    else {
-    
-    //choose SE
-    
-    d = d + 2*x - 2*y + 5;
-    
-    x = x + 1;
-    
-    y = y - 1;
-    pushDirection("SE", d) 
-    }
-    
-    drawPoints(x+x1, y+y1, MIDX, MIDY)
-    
-    }
+
+    drawPoints(x , y , circX, circY);
+  }
+  return [x1,y1]
 }
-  
-function showResultsCircle(){
+
+function showResultsCircle(center) {
   results.innerHTML = `<tr>  
   <th>d</th>
   <th>Direction</th>
@@ -199,29 +192,89 @@ function showResultsCircle(){
   <th>Zone 6</th>
   <th>Zone 7</th>
   <th>Zone 8</th>
-</tr>` 
-  for (let i = 0 ; i < MIDX.length ; i++) {
+</tr>`;
+  for (let i = 0; i < circX.length; i++) {
     results.innerHTML += `<tr>
-    <td>${d[i]}</td>
-    <td>${dir[i]}</td>
-    <td>(${MIDX[i]},${MIDY[i]})</td>
-    <td>(${MIDX[i]},-${MIDY[i]})</td> 
-    <td>(-${MIDX[i]},-${MIDY[i]})</td> 
-    <td>(-${MIDX[i]},${MIDY[i]})</td> 
-    <td>(${MIDY[i]},${MIDX[i]})</td> 
-    <td>(-${MIDY[i]},${MIDX[i]})</td> 
-    <td>(-${MIDY[i]},-${MIDX[i]})</td> 
-    <td>(${MIDY[i]},-${MIDX[i]})</td> 
+  <td>${d[i]}</td>
+  <td>${dir[i]}</td>
+  <td>(${circX[i]+center[0]},${circY[i]+center[1]})</td>
+  <td>(${circX[i]+center[0]},${(-1 * circY[i])+center[1]})</td> 
+  <td>(${(-1 * circX[i])+center[0]},${(-1 * circY[i])+center[1]})</td> 
+  <td>(${(-1 * circX[i])+center[0]},${circY[i]+center[1]})</td> 
+  <td>(${circY[i]+center[0]},${circX[i]+center[1]})</td> 
+  <td>(${-1 * circY[i]+center[0]},${circX[i]+center[1]})</td> 
+  <td>(${(-1 * circY[i])+center[0]},${(-1 * circX[i])+center[1]})</td> 
+  <td>(${circY[i]+center[0]},${(-1 * circX[i]+center[1])})</td> 
 
-    </tr>`
+  </tr>`;
   }
 }
 
+function plotValues(X, Y) {
+  var trace1 = {
+    x: X,
+    y: Y,
+    mode: "markers",
+    type: "scatter",
+  };
+
+  var data = [trace1];
+  var layout = {
+    yaxis: {
+      scaleanchor: "x",
+    }
+  }
+  Plotly.newPlot("plot", data,layout);
+}
+
+function plotCircle(X, Y,center) {
+  console.log(center)
+  Z = X.length;
+  for (let i = 0; i < Z; i++) {
+
+    X.push(X[i]+center[0]);
+    X.push((-1 * X[i])+center[0]);
+    X.push((-1 * X[i])+center[0]);
+    X.push(Y[i]+center[0]);
+    X.push((-1 * Y[i])+center[0]);
+    X.push((-1 * Y[i])+center[0]);
+    X.push(Y[i]+center[0]);
+
+    Y.push((-1 * Y[i])+center[1]);
+    Y.push((-1 * Y[i])+center[1]);
+    Y.push(Y[i]+center[1]);
+    Y.push(X[i]+center[1]);
+    Y.push(X[i]+center[1]);
+    Y.push((-1 * X[i])+center[1]);
+    Y.push((-1 * X[i])+center[1]);
+    X[i]+=center[0]
+    Y[i]+=center[1]
+  }
+
+  console.log(X);
+  console.log(Y);
+
+  var coordinates = {
+    x: X,
+    y: Y,
+    name: 'Co-ordinates',
+    mode: "markers",
+    type: "scatter",
+  };
+  var centerPoint = {
+    x: [center[0]],
+    y: [center[1]],
+    name: 'Center',
+    mode: 'markers',
+    type: 'scatter'
+  }
+  var data = [coordinates,centerPoint];
 
 
-// console.log(DDAX) 
-// console.log(DDAY) 
-console.log(MIDX) 
-console.log(MIDY) 
-console.log(dir) 
-console.log(d) 
+  var layout = {
+    yaxis: {
+      scaleanchor: "x",
+    }
+  }
+  Plotly.newPlot("plot", data,layout);
+}
--- util.js
+++ util.js
@@ -6,24 +6,11 @@ const ddaIN = document.getElementById('dda-input')
 const midpointIN = document.getElementById('midp-input')
 const circleIN = document.getElementById('circle-input')
 
-
-// console.log("in util")
-
 function toggleInput(button,inputDOM){
     button.onclick=()=>{
-    console.log("clicked")
     inputDOM.hidden = inputDOM.hidden ? false : true
 }
 }
-
-// function showResultsDDA(){
-//  results.innerHTML=``
-// for(let i =0;i<DDAX.length;i++  ){
-//     results.innerHTML+=`(${DDAX[i]},${DDAY[i]})`
-// }
-
-// }
-
 
 toggleInput(dda,ddaIN)
 toggleInput(midpoint,midpointIN)
